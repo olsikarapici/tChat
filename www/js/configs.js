6f3f52e72b904212e17ab754b7aae63038e046c0
Olsi <olsikarapici@gmail.com>
2018-04-30 14:00:17 +0200

    css fixes, logo for the app, fixing for facebook authentication with firebase

---
+++ www/js/configs.js
@@ -0,0 +1,6 @@
+angular.module('Chat.configs', [])
+
+.constant("CONFIG", {
+  // "FIREBASE_URL": 'https://ionic-real-chat.firebaseio.com'
+  "FIREBASE_URL": 'https://tchat-105b9.firebaseio.com'
+});
---
+++ www/js/init_firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBCt29he-DVqnGwhU_1dZ_pV584liQYMno",
+  authDomain: "tchat-105b9.firebaseapp.com",
+  databaseURL: "https://tchat-105b9.firebaseio.com",
+  projectId: "tchat-105b9",
+  storageBucket: "tchat-105b9.appspot.com",
+  messagingSenderId: "866220362559"
+};
+
+
+firebase.initializeApp(firebaseConfig);
